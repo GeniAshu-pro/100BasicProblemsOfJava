c2898a3f459428757814dd4e39d943dc27ad1251
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-08 14:49:03 +0530

    Uploads of Problems 1 to 16

---
+++ src/Probs1To16.java
@@ -0,0 +1,179 @@
+import java.util.Scanner;
+
+public class Probs1To16 {
+    public static void main(String[] args) {
+
+//        Area of Circle problem and Circumference
+//        System.out.println("Enter the radius of the circle");
+//        Scanner sc = new Scanner (System.in );
+//        int r = sc.nextInt();
+//        float pi = 3.14f;
+//        float  area_Ans = pi * r * r;
+//        float circum_Ans = 2* pi *r;
+//        System.out.println("The Area of Circle is  "+  area_Ans );
+//        System.out.println("The Circumference of Circle is  "+  circum_Ans );
+
+//        Area of rectangle and perimeter
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the length of rectangle");
+//        int l = sc.nextInt();
+//        System.out.println("Enter the breadth of rectangle");
+//        int b = sc.nextInt();
+//        int z = l * b;
+//        int y = 2*(l * b);
+//        System.out.println("The Area of rectangle " + z);
+//        System.out.println("The perimeter of rectangle " + y);
+
+//        Swapping number using third variable
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Any Number between 0-9");
+//        int a = sc.nextInt();
+//        System.out.println("Again enter any number between 0-9");
+//        int b = sc.nextInt();
+//        int c = a;
+//        a=b;
+//        b=c;
+//        System.out.println("Number you Entered in b is now swap with a : "+a);
+//        System.out.println("Number you Entered in a is now swap with b : "+b);
+
+//        Celsius into fahrenheit
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Temperature you wanna convert into fahrenheit");
+//        int c = sc.nextInt();
+//        float f  = (c*9/5f)+32 ;
+//        System.out.println("The Temperature in Fahrenheit is: "+f);
+
+//        Average of 5 subject
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("How many Subject U have");
+//        int n = sc.nextInt();
+//        System.out.println("Enter marks u scored in English");
+//        int sub_1 = sc.nextInt();
+//        System.out.println("Enter marks u scored in Hindi");
+//        int sub_2 = sc.nextInt();
+//        System.out.println("Enter marks u scored in Science");
+//        int sub_3 = sc.nextInt();
+//        System.out.println("Enter marks u scored in Math");
+//        int sub_4 = sc.nextInt();
+//        System.out.println("Enter marks u scored in GK");
+//        int sub_5 = sc.nextInt();
+//        int total_mrks = sub_1+sub_2+sub_3+sub_4+sub_5;
+//        float Avg_mrks ;
+//        Avg_mrks = total_mrks/n ;
+//        System.out.println("The total marks u have Scored "+total_mrks);
+//        System.out.println("The Average marks u Got is "+Avg_mrks);
+
+//        Even or odd
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Number here !!");
+//        int n = sc.nextInt();
+//        if(n%2==0){
+//            System.out.println("The Entered number is Even");
+//        }
+//        else{
+//            System.out.println("The Entered Number is Odd");
+//        }
+
+        // Positive , Negative , zero
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Number here !!");
+//        int n = sc.nextInt();
+//        if(n>0){
+//            System.out.println("The Entered number is Positive");
+//        }
+//        else{
+//            if(n==0){
+//                System.out.println("The Entered number is Zero");
+//            }
+//            else{
+//                System.out.println("The Entered number is Negative");
+//            }
+//        }
+
+//        finding largest among
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 1st Number");
+//        int a = sc.nextInt();
+//        System.out.println("Enter 2nd Number");
+//        int b =sc.nextInt();
+//        System.out.println("Enter 3rd Number");
+//        int c = sc.nextInt();
+//         if (a>b){
+//             if(a>c){
+//                 System.out.println("The largest number is : "+a);
+//             }
+//             else{
+//                 System.out.println("The Number  "+a+"  is not a largest number ");
+//             }
+//         }
+//         else {
+//             if (b > c) {
+//                 System.out.println("The largest number is : " + b);
+//             } else {
+//                 System.out.println("The largest number is : " + c);
+//             }
+//         }
+
+
+//        finding the smallest among three
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter 1st Number");
+//        int a = sc.nextInt();
+//        System.out.println("Enter 2nd Number");
+//        int b =sc.nextInt();
+//        System.out.println("Enter 3rd Number");
+//        int c = sc.nextInt();
+//
+
+
+
+
+//        leap year or not
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter  year here !!");
+//        int n = sc.nextInt();
+//        if(n%4==0){
+//            System.out.println("The entered year is leap year");
+//        }
+//        else{
+//            if (n%400==0){
+//                System.out.println("The entered year is leap year");
+//            }
+//            else {
+//                System.out.println("The entered year is not a leap year");
+//            }
+//        }
+
+
+//        character checking if its vowel or not
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter character here!!");
+//        char ch = sc.next().charAt(0);
+//        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ){
+//            System.out.println("Entered character is Vowel");
+//        } else if (ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' ) {
+//            System.out.println("Entered character is Vowel");
+//        }
+//        else{
+//            System.out.println("Entered character is consonant ");
+//        }
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
