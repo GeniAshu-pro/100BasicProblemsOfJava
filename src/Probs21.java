a9dac2587666779175df315f8a9fb31c1c84ff2c
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-09 02:06:52 +0530

    Add ProblemNo: 21

--- src/Probs21.java
+++ src/Probs21.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+// here  We are solving Sum of all natural number
 public class Probs21 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number here ");
+        int number = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i <= number; i++) {
+            sum = sum + i;
+            System.out.println(sum);
+        }
 
     }
 }
