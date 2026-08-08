d756c4c1bacef6c34804f6d154439697d55557b0
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-09 02:26:47 +0530

    Add probs 23

---
+++ src/Probs23.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+// Here at the Place of even Number  we will display Even
+public class Probs23 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number here !!! ");
+        int number = sc.nextInt();
+    for (int i =1 ; i<=number;i++){
+        if (i%2==0){
+            System.out.println("Even");
+        }else {
+            System.out.println(i);
+        }
+
+    }
+    }
+}
