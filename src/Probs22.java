c5df8b11700d2db3a4c72989e2902fbff38923b2
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-09 02:06:23 +0530

    Add ProblemNo: 22

---
+++ src/Probs22.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Probs22 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter Number here !!");
+        int n =sc.nextInt();
+        for(int i = n ;i>=1;i--){
+            System.out.println(i);
+        }
+    }
+}
