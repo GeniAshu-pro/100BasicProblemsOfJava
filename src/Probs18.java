3616aa1c98cb2b055217a1ce367f6b4a34594116
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-08 17:18:12 +0530

    Add ProblemNo: 18

--- src/Probs18.java
+++ src/Probs18.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Probs18 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+        if (marks < 0 || marks > 100) {
+            System.out.println("Invalid Marks");
+            return;
+        }
+        if (marks >= 80) {
+            System.out.println("Grade A");
+        } else if (marks >= 70) {
+            System.out.println("Grade B");
+        } else if (marks >= 60) {
+            System.out.println("Grade C");
+        } else if (marks >= 50) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("FAIL");
+        }
+        sc.close();
     }
 }
