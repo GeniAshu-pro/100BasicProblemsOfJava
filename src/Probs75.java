df1f24c81e6bfb645b3d44a45b29179d94cc273a
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-16 00:28:13 +0530

    Probs69

--- src/Probs75.java
+++ src/Probs75.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class Probs75 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String newString  =str.replaceAll(" ","");
+        System.out.println(newString);
+    }
 }
