c0aa7476e41c855f3e10e0dc587339a72a3e6cc0
GeniAshu-pro <pandeyashutosh67215@gmail.com>
2026-08-16 00:18:05 +0530

    Probs69

--- src/Probs69.java
+++ src/Probs69.java
@@ -5,19 +5,21 @@ public class Probs69 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your String here !!");
         String str = sc.nextLine();
+        String cleaned = str.replaceAll("[^A-Za-z]", "");
+        int count = 0;
+        int consonants = 0;
+        int n = str.length();
 
-
-
-
-
-
-
-
-
-
-
-
-
+        for (int i = 0 ; i<n ;i++){
+            char ch = str.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                count++;
+            } else {
+                consonants++;
+            }
+        }
+        System.out.println("The Number of Vowels is : " + count);
+        System.out.println("The Number of Consonants is : " + consonants);
 
     }
 }
